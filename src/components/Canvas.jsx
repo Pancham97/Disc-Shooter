cd2bc18e2a741cbfc15ad4d7f8fa591eb9b6f09b
panchamk <talktopancham@gmail.com>
2018-12-20 14:58:44 +0530

    Updated the code to respond to the start game effect and created new assets of the game.

--- src/components/Canvas.jsx
+++ src/components/Canvas.jsx
@@ -4,14 +4,16 @@ import Ground from './Ground';
 import CannonPipe from './CannonPipe';
 import CannonBase from './CannonBase';
 import PropTypes from 'prop-types';
+import CannonBall from './CannonBall';
+import CurrentScore from './CurrentScore';
+import FlyingObject from './FlyingObject';
+import Heart from './Heart';
+import StartGame from './StartGame';
+import Title from './Title';
 
 const Canvas = (props) => {
-    const style = {
-        border: '1px solid #000',
-    };
-
-    const viewBox = [window.innerWidth / -2, 100 - window.innerHeight, window.innerWidth, window.innerHeight];
-
+    const gameHeight = 1200;
+    const viewBox = [window.innerWidth / -2, 100 - gameHeight, window.innerWidth, gameHeight];
 
     return(
         <svg
@@ -19,11 +21,33 @@ const Canvas = (props) => {
             preserveAspectRatio="xMaxYMax none"
             onMouseMove={props.trackMouse}
             viewBox={viewBox} >
+
+            <defs>
+                <filter id="shadow">
+                    <feDropShadow dx="1" dy="1" stdDeviation="2" />
+                </filter>
+            </defs>
             <Sky />
             <Ground />
             <CannonPipe rotation={props.angle} />
             <CannonBase />
-            {/* <circle cx={0} cy={0} r={50} /> */}
+            <CannonBall position={{x: 0, y: -100}} />
+            <CurrentScore score={15} />
+            {/* <Heart position={{x: -300, y: 35}} /> */}
+            { ! props.gameState.started &&
+                <g>
+                    <StartGame onClick={() => props.startGame()} />
+                </g>
+            }
+
+            { props.gameState.started && 
+                <g>
+                    <FlyingObject position={{x: -150, y: -300}} />
+                    <FlyingObject position={{x: 150, y: -300}} />
+                </g>
+            }
+            
+            <Title />
         </svg>
     );
 };
@@ -31,6 +55,12 @@ const Canvas = (props) => {
 Canvas.propTypes = {
     angle: PropTypes.number.isRequired,
     trackMouse: PropTypes.func.isRequired,
+    gameState: PropTypes.shape({
+        started: PropTypes.bool.isRequired,
+        kills: PropTypes.number.isRequired,
+        lives: PropTypes.number.isRequired,
+    }).isRequired,
+    startGame: PropTypes.func.isRequired,
 };
 
 export default Canvas;
